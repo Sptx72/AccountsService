f80ccba3566b7397173d40ca77929a9094db9b24
Marco Moncada <jj4595702@gmail.com>
2024-02-15 12:06:44 +0100

    first commit

---
+++ src/main/java/com/microcompany/accountsservice/controller/AccountController.java
@@ -0,0 +1,12 @@
+package com.microcompany.accountsservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+
+
+}
---
+++ src/main/java/com/microcompany/accountsservice/filter/LoggingFilter.java
@@ -0,0 +1,32 @@
+package com.microcompany.accountsservice.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@WebFilter
+public class LoggingFilter implements Filter {
+
+    private final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("Request: " + servletRequest.getServerName());
+        log.info("Response: " + servletResponse);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
