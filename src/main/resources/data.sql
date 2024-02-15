3b6d84125d2ed54c1ede1b06c0987fae2edf4774
admin <j.mestreferrandez@dxc.com>
2024-02-15 15:36:42 +0100

    WIP fixed data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,4 +8,4 @@ CREATE TABLE accounts (
   owner_id BIGINT
   );
 
-INSERT INTO ACCOUNTS (id, type, opening_date, balance, owner_id) VALUES (1, 'PREMIUM', NULL, 17, 1),(2, 'DEFAULT', NULL, 17, 2),(3, 'PREMIUM', NULL, 17, 3),(4, 'DEFAULT', NULL, 17, 4),(5, 'PREMIUM', NULL, 17, 5),(6, 'DEFAULT', NULL, 17, 6),(7, 'PREMIUM', NULL, 17, 7)
+INSERT INTO ACCOUNTS (type, opening_date, balance, owner_id) VALUES ('PREMIUM', NULL, 17, 1),('DEFAULT', NULL, 17, 2),('PREMIUM', NULL, 17, 3),('DEFAULT', NULL, 17, 4),('PREMIUM', NULL, 17, 5),('DEFAULT', NULL, 17, 6),('PREMIUM', NULL, 17, 7)
