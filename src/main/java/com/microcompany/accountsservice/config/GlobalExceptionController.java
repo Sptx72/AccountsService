dd85600175bc3de1393cd17f8a9131c64e80263b
admin <j.mestreferrandez@dxc.com>
2024-02-20 12:11:42 +0100

    WIP

---
+++ src/main/java/com/microcompany/accountsservice/config/GlobalExceptionController.java
@@ -0,0 +1,20 @@
+package com.microcompany.accountsservice.config;
+
+import com.microcompany.accountsservice.exception.AccountNotfoundException;
+import com.microcompany.accountsservice.exception.GlobalException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionController {
+ @ExceptionHandler(GlobalException.class)
+ ResponseEntity<Object> noSuchElementExceptionHandler(GlobalException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+     @ExceptionHandler(AccountNotfoundException.class)
+    ResponseEntity<Object> noSuchElementExceptionHandler(AccountNotfoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Excepcion de cuenta");
+    }
+}
--- src/main/java/com/microcompany/accountsservice/controller/AccountController.java
+++ src/main/java/com/microcompany/accountsservice/controller/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,6 +19,7 @@ import java.util.List;
 @Tag(name = "Accounts API", description = "Accounts management APIs")
 @RestController
 @RequestMapping("/accounts")
+@Validated
 public class AccountController {
 
     @Autowired
